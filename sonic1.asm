97de01a54e19c89e88928a0a619abf6656649c25
Jacob T. Andersen <jacobjaretts@gmail.com>
2021-05-17 19:22:52 -0700

    Revert "major update"
    This reverts commit 540f8b5c113a812125a525d4948ca684c42b5a77.

--- sonic1.asm
+++ sonic1.asm
@@ -3341,9 +3341,9 @@ SegaScreen:				; XREF: GameModeArray
 		andi.b	#$BF,d0
 		move.w	d0,(VDP_CTRL).l
 		bsr.w	ClearScreen
-		lea	(Twim_SegaLogo).l,a0 ; load Sega	logo patterns
-		moveq	#0,d0
-		jsr	TwimDec
+		move.l	#$40000000,(VDP_CTRL).l
+		lea	(Nem_SegaLogo).l,a0 ; load Sega	logo patterns
+		bsr.w	NemDec
 		lea	($FF0000).l,a1
 		lea	(Eni_SegaLogo).l,a0 ; load Sega	logo mappings
 		move.w	#0,d0
@@ -3551,10 +3551,11 @@ Title_LoadText:
 		bsr.w	DeformBgLayer
 		lea	($FFFFB000).w,a1
 		lea	(Blk16_GHZ).l,a0 ; load	GHZ 16x16 mappings
-		jsr	TwizDec
+		move.w	#0,d0
+		bsr.w	EniDec
 		lea	(Blk256_GHZ).l,a0 ; load GHZ 256x256 mappings
 		lea	($FF0000).l,a1
-		jsr	TwizDec
+		bsr.w	KosDec
 		bsr.w	LevelLayoutLoad
 		bsr.w	Pal_FadeFrom
 		bsr.w	ClearScreen
@@ -9028,10 +9029,10 @@ MainLoadBlockLoad:			; XREF: Level; EndingSequence
 		movea.l	(a2)+,a0
 		lea	($FFFFB000).w,a1 ; RAM address for 16x16 mappings
 		move.w	#0,d0
-		jsr	TwizDec
+		bsr.w	EniDec
 		movea.l	(a2)+,a0
 		lea	($FF0000).l,a1	; RAM address for 256x256 mappings
-		jsr	TwizDec
+		bsr.w	KosDec
 		bsr.w	LevelLayoutLoad
 		move.w	(a2)+,d0
 		move.w	(a2),d0
@@ -38924,17 +38925,17 @@ MainLoadBlocks:
 ArtLoadCues:
 	include "_inc\Pattern load cues.asm"
 
-Twim_SegaLogo:	incbin	arttwim\segalogo.twim	; large Sega logo
+Nem_SegaLogo:	incbin	artnem\segalogo.bin	; large Sega logo
 		even
 Eni_SegaLogo:	incbin	mapeni\segalogo.bin	; large Sega logo (mappings)
 		even
 Eni_Title:	incbin	mapeni\titlescr.bin	; title screen foreground (mappings)
 		even
-Twim_TitleFg:	incbin	arttwim\titlefor.twim	; title screen foreground
+Twim_TitleFg:	incbin	arttwim\titlefor.bin	; title screen foreground
 		even
-Twim_TitleSonic:	incbin	arttwim\titleson.twim	; Sonic on title screen
+Twim_TitleSonic:	incbin	arttwim\titleson.bin	; Sonic on title screen
 		even
-Twim_TitleTM:	incbin	arttwim\titletm.twim	; TM on title screen
+Twim_TitleTM:	incbin	arttwim\titletm.bin	; TM on title screen
 		even
 Eni_JapNames:	incbin	mapeni\japcreds.bin	; Japanese credits (mappings)
 		even
@@ -39239,43 +39240,43 @@ Nem_Squirrel:	incbin	artnem\squirrel.bin	; squirrel
 ; ---------------------------------------------------------------------------
 ; Compressed graphics - primary patterns and block mappings
 ; ---------------------------------------------------------------------------
-Blk16_GHZ:	incbin	map16\ghz.twiz
+Blk16_GHZ:	incbin	map16\ghz.bin
 		even
 Nem_GHZ_1st:	incbin	artnem\8x8ghz1.bin	; GHZ primary patterns
 		even
 Nem_GHZ_2nd:	incbin	artnem\8x8ghz2.bin	; GHZ secondary patterns
 		even
-Blk256_GHZ:	incbin	map256\ghz.twiz
+Blk256_GHZ:	incbin	map256\ghz.bin
 		even
-Blk16_LZ:	incbin	map16\lz.twiz
+Blk16_LZ:	incbin	map16\lz.bin
 		even
 Nem_LZ:		incbin	artnem\8x8lz.bin	; LZ primary patterns
 		even
-Blk256_LZ:	incbin	map256\lz.twiz
+Blk256_LZ:	incbin	map256\lz.bin
 		even
-Blk16_MZ:	incbin	map16\mz.twiz
+Blk16_MZ:	incbin	map16\mz.bin
 		even
 Nem_MZ:		incbin	artnem\8x8mz.bin	; MZ primary patterns
 		even
-Blk256_MZ:	incbin	map256\mz.twiz
+Blk256_MZ:	incbin	map256\mz.bin
 		even
-Blk16_SLZ:	incbin	map16\slz.twiz
+Blk16_SLZ:	incbin	map16\slz.bin
 		even
 Nem_SLZ:	incbin	artnem\8x8slz.bin	; SLZ primary patterns
 		even
-Blk256_SLZ:	incbin	map256\slz.twiz
+Blk256_SLZ:	incbin	map256\slz.bin
 		even
-Blk16_SYZ:	incbin	map16\syz.twiz
+Blk16_SYZ:	incbin	map16\syz.bin
 		even
 Nem_SYZ:	incbin	artnem\8x8syz.bin	; SYZ primary patterns
 		even
-Blk256_SYZ:	incbin	map256\syz.twiz
+Blk256_SYZ:	incbin	map256\syz.bin
 		even
-Blk16_SBZ:	incbin	map16\sbz.twiz
+Blk16_SBZ:	incbin	map16\sbz.bin
 		even
 Nem_SBZ:	incbin	artnem\8x8sbz.bin	; SBZ primary patterns
 		even
-Blk256_SBZ:	incbin	map256\sbz.twiz
+Blk256_SBZ:	incbin	map256\sbz.bin
 		even
 ; ---------------------------------------------------------------------------
 ; Compressed graphics - bosses and ending sequence
